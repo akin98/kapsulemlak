5514e353f444025a753a1a623a0633532a907a27
akın sultar <109798864+akin98@users.noreply.github.com>
2022-07-22 14:28:32 +0300

    kapsul emlak
    kapsul emlak örneği

---
+++ kapsül emlak örneği/kapsül emlak örneği/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kapsül_emlak_örneği
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            emlak a = new emlak();
+            a.SEHİR = "istanbul";
+            a.MAH = "akalan";
+            a.KAT = -2;
+            a.ODA = -5;
+            a.KİRA = 1500;
+            Console.WriteLine(a.SEHİR);
+            Console.WriteLine(a.MAH);
+            Console.WriteLine(a.KAT);
+            Console.WriteLine(a.ODA);
+            Console.WriteLine(a.KİRA);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ kapsül emlak örneği/kapsül emlak örneği/emlak.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kapsül_emlak_örneği
+{
+    internal class emlak
+    {
+        private string şehir;
+        private string mahalle;
+        private int kat;
+        private int odasayısı;
+        private int kira;
+
+        public string SEHİR
+        {
+            get { return şehir; }
+            set { şehir = value.ToLower(); }
+        }
+        public string MAH
+        {
+            get { return mahalle; }
+            set { mahalle = value.ToLower(); }
+        }
+        public int KAT
+        {
+            get { return kat; }
+            set
+            {
+                if (value < 0)
+                {
+                    kat = Math.Abs(value);
+                }
+                else
+                {
+                    kat = value;
+                }
+            }
+        }
+        public int ODA
+        {
+            get { return odasayısı; }
+            set
+            {
+                if (value < 0)
+                {
+                    odasayısı = Math.Abs(value);
+                }
+                else
+                {
+                    odasayısı = value;
+                }
+            }
+        }
+        public int KİRA
+        {
+            get { return kira; }
+            set
+            {
+                if(value<2500)
+                {
+                    kira = 2500;
+                }
+                else
+                {
+                    kira = value;
+                }
+            }
+        }
+    }
+}
